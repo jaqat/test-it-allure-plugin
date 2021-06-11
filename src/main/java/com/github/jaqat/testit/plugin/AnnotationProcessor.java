c6790a7413408a9e9526f90beb29e0f855cef629
aafrikanov <afrikanov.andrey@gmail.com>
2021-06-11 14:45:07 +0300

    Test-It idea plugin: v1.1

--- src/main/java/com/github/jaqat/testit/plugin/AnnotationProcessor.java
+++ src/main/java/com/github/jaqat/testit/plugin/AnnotationProcessor.java
@@ -21,6 +21,9 @@ public class AnnotationProcessor {
     static final String ALLURE_LINKS_ANNOTATION = "io.qameta.allure.Links";
     static final String ALLURE_TMS_LINK_ANNOTATION = "io.qameta.allure.TmsLink";
 
+    static final String JUNIT5_TEST_ANNOTATION = "org.junit.jupiter.api.Test";
+    static final String TESTNG_TEST_ANNOTATION = "org.testng.annotations.Test";
+
     private static PsiAnnotation createTmsLinkAnnotation(PsiMethod method, String autotestUuid) {
         return PsiUtils.createAnnotation(
                 format("@%s(\"%s\")", ALLURE_TMS_LINK_ANNOTATION, autotestUuid),
@@ -56,18 +59,29 @@ public class AnnotationProcessor {
                 autotestSettings.getManualTestsIds()
         );
 
-        //PsiAnnotation displayNameAnnotation = testMethod.getAnnotation(JUNIT_DISPLAY_NAME_ANNOTATION);
+        PsiAnnotation testAnnotation = getTestAnnotation(testMethod);
+        if (testAnnotation != null) {
 
-        CommandProcessor.getInstance().executeCommand(
-                testMethod.getProject(),
-                () -> ApplicationManager.getApplication().runWriteAction(() -> {
-                    PsiUtils.addImport(testMethod.getContainingFile(), ALLURE_TMS_LINK_ANNOTATION);
-                    PsiUtils.addImport(testMethod.getContainingFile(), ALLURE_LINK_ANNOTATION);
-                    testMethod.getModifierList().add(tmsLinkAnnotation);
-                    linkAnnotations.forEach(annotation ->testMethod.getModifierList().add(annotation));
-                }),
-                "Create Autotest and add @TmsLink annotation with created UUID",
-                null
-        );
+            CommandProcessor.getInstance().executeCommand(
+                    testMethod.getProject(),
+                    () -> ApplicationManager.getApplication().runWriteAction(() -> {
+                        PsiUtils.addImport(testMethod.getContainingFile(), ALLURE_TMS_LINK_ANNOTATION);
+                        PsiUtils.addImport(testMethod.getContainingFile(), ALLURE_LINK_ANNOTATION);
+                        linkAnnotations.forEach(annotation -> testMethod.getModifierList().addAfter(annotation, testAnnotation));
+                        testMethod.getModifierList().addAfter(tmsLinkAnnotation, testAnnotation);
+                    }),
+                    "Create Autotest and add @TmsLink annotation with created UUID",
+                    null
+            );
+        }
+    }
+
+    public static PsiAnnotation getTestAnnotation(PsiMethod testMethod){
+           PsiAnnotation testAnnotation = testMethod.getAnnotation(JUNIT5_TEST_ANNOTATION);
+           if (testAnnotation == null){
+               testAnnotation = testMethod.getAnnotation(TESTNG_TEST_ANNOTATION);
+           }
+
+        return testAnnotation;
     }
 }
